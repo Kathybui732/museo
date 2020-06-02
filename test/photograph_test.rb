c7e52273866d6f4e3bc56778cf206e43d9f4b1cd
Kathy Bui <kathy.bui1992@gmail.com>
2020-06-02 17:22:20 -0600

    Create test for photograph with attributes

--- test/photograph_test.rb
+++ test/photograph_test.rb
@@ -1,26 +1,24 @@
-require 'minitest'
-require 'minitest/autorun'
-require 'minitest/pride'
-require './lib/photograph'
+require "minitest/autorun"
+require "minitest/pride"
+require "./lib/photograph"
 
-class PhotographTest < Minitest::Test
-  def test_it_exists
-    photograph = Photograph.new({})
-    assert_instance_of Photograph, photograph
-  end
+class PhotographTest < MiniTest::Test
 
-  def test_it_has_attributes
+	def setup
     attributes = {
       id: "1",
       name: "Rue Mouffetard, Paris (Boy with Bottles)",
-      artist_id: 4,
-      year: 1954
+      artist_id: "4",
+      year: "1954"
     }
-    photograph = Photograph.new(attributes)
+    @photograph = Photograph.new(attributes)
+	end
 
-    assert_equal "1", photograph.id
-    assert_equal "Rue Mouffetard, Paris (Boy with Bottles)", photograph.name
-    assert_equal 4, photograph.artist_id
-    assert_equal 1954, photograph.year
-  end
+	def test_it_exists_with_attributes
+		assert_instance_of Photograph, @photograph
+    assert_equal "1", @photograph.id
+    assert_equal "Rue Mouffetard, Paris (Boy with Bottles)", @photograph.name
+    assert_equal "4", @photograph.artist_id
+    assert_equal "1954", @photograph.year
+	end
 end
