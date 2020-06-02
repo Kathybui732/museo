76a7aac795c8b854fdce77ab108551b202007c1d
Kathy Bui <kathy.bui1992@gmail.com>
2020-06-02 17:53:18 -0600

    Create method for photographs by artists

--- lib/curator.rb
+++ lib/curator.rb
@@ -20,4 +20,18 @@ class Curator
       artist.id == id
     end
   end
+
+  def photographs_by_id(id)
+    @photographs.select do |photo|
+      photo.artist_id == id
+    end
+  end
+
+  def photographs_by_artist
+    photographs_by_artist = Hash.new
+    @artists.each do |artist|
+      photographs_by_artist[artist] = photographs_by_id(artist.id)
+    end
+    photographs_by_artist
+  end
 end
